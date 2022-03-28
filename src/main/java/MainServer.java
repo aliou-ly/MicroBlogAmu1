cd18b96f6172c32d173ec090d1db63b4252dea94
Loop-afk <kevinp966@gmail.com>
2022-03-28 20:15:42 +0200

    Ajout des messages dans un fichier json
    TODO - Génération id par le serveur

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -4,7 +4,10 @@ import org.json.JSONObject;
 import servers.HandleServer;
 import users.Users;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 
 public class MainServer {
@@ -12,7 +15,7 @@ public class MainServer {
 
 
     public static void main(String[] args) throws IOException {
-        JSONObject test = new JSONObject();
+/*        JSONObject test = new JSONObject();
         test.put("author","testUser");
         test.put("msgId","sjwdkjw");
         test.put("limit",5);
@@ -25,6 +28,40 @@ public class MainServer {
         JSONArray msg = new JSONArray();
         msg.put(test);
         msg.put(test2);
-        System.out.println(msg);
+        System.out.println(msg);*/
+
+        //Test
+
+        JSONObject author = new JSONObject();
+        author.put("author", "test");
+
+        JSONObject author2 = new JSONObject();
+        author2.put("author", "test");
+
+        JSONObject messageDetails = new JSONObject();
+        messageDetails.put("msgId", "okok");
+        messageDetails.put("txt","blablabla");
+
+        JSONObject messageDetails2 = new JSONObject();
+        messageDetails2.put("msgId", "okokok");
+        messageDetails2.put("txt","miamiamiam");
+
+        //Add message to list
+        JSONArray message = new JSONArray();
+        message.put(messageDetails);
+        message.put(messageDetails2);
+
+
+        author.put("Message List", message);
+
+        //Write JSON file
+        try (FileWriter file = new FileWriter("test.json",true)) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(author.toString(4)+",\n");
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/servers/queryprocessing/PublishQueryProcessing.java
+++ src/main/java/servers/queryprocessing/PublishQueryProcessing.java
@@ -1,6 +1,11 @@
 package servers.queryprocessing;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import servers.queryprocessing.AbstractQueryProcessing;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class PublishQueryProcessing extends AbstractQueryProcessing {
 
@@ -8,5 +13,33 @@ public class PublishQueryProcessing extends AbstractQueryProcessing {
     @Override
     public void executeProcess() {
         separate();
+
+        // Author Section
+        JSONObject author = new JSONObject();
+        author.put("author", this.header);
+
+        // Structure of the message - TODO - Generate the id
+        JSONObject messageDetails = new JSONObject();
+        messageDetails.put("msgId", "test");
+        messageDetails.put("txt",this.message);
+
+        //Add message to list
+        // L'idée aurait normalement été d'interroger s'il l'auteur est déjà dans notre fichier que ça rajoute
+        // le message à la suite de la liste déjà existante.
+        JSONArray message = new JSONArray();
+        message.put(messageDetails);
+        // Attach the messages to the user
+        author.put("Message List", message);
+
+        //Write JSON file
+        try (FileWriter file = new FileWriter("test.json",true)) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(author.toString(4)+",\n");
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
