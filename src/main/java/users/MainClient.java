a6193afeb5246ba100475c1968e5effa2e7b5afc
Loop-afk <kevinp966@gmail.com>
2022-04-01 23:51:47 +0200

    Fin de projet

--- src/main/java/users/MainClient.java
+++ src/main/java/users/MainClient.java
@@ -3,13 +3,17 @@ package users;
 import factories.Factory;
 import requests.Requests;
 
-import java.io.File;
 import java.io.IOException;
 
 public class MainClient {
     public static void main(String[] args) throws IOException {
         Users<String> testUser = new NetworkUser("@testUser5","localhost",12345);
-        Requests requests = new Factory().createReplyMSGRequest(testUser);
+
+        Requests requests = new Factory().createPublishRequest(testUser);
         testUser.shareMessage(requests);
+
+        //Requests requests1 = new Factory().createRepublishRequest(testUser);
+        //testUser.shareMessage(requests1);
+        }
+
     }
-}
